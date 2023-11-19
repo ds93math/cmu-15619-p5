065ffc93ba35158ea60138b47d2adc49466af36f
ds93math <su.david2016@gmail.com>
2023-11-19 01:14:14 -0800

    ;

--- stream-processing/DataProducer/src/main/java/DataProducer.java
+++ stream-processing/DataProducer/src/main/java/DataProducer.java
@@ -34,7 +34,7 @@ public class DataProducer {
                 JSONObject json = new JSONObject(line);
                 String type = json.getString("type");
                 String topic = determineTopic(type);
-                String key = null // String.valueOf(json.getInt("blockId")); // Partition based on blockId
+                String key = null; // String.valueOf(json.getInt("blockId")); // Partition based on blockId
                 Integer my_partition = json.getInt("blockId") % 5;
                 producer.send(new ProducerRecord<>(topic, my_partition, key, line));
             }
